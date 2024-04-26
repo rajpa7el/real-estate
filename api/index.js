b56c8097c5969126337b7135f87ebdcf8c611885
rajpa7el <rajpatel112003@gmail.com>
2024-04-26 20:05:50 -0230

    create user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on Port 3000!");
+    console.log("Server is running on Port 3000");
 });
